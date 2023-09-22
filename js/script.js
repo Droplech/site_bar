211c4b6ceba51210e22f98297f9bb38e675ff240
Maxim <53229586+Droplech@users.noreply.github.com>
2023-09-22 20:18:59 +0300

    +1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function openRightMenu() {
     document.getElementById("rightMenu").style.display = "block";
-    console.log('1')
+    
 }
 
 function closeRightMenu() {
     document.getElementById("rightMenu").style.display = "none";
-    console.log('2')
+    
 }
