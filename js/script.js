1530c8fc2a8b18673582ce065037f22ad103216a
Maxim <53229586+Droplech@users.noreply.github.com>
2023-09-25 15:11:36 +0300

    commit 3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,45 @@
-// function openRightMenu() {
-//     document.getElementById("rightMenu").style.display = "block";
+$('.burger').click(function(){
+    $('.burger_menu').addClass('burger_menu-active')
+    $('.menu_background').show()
+})
+$('.burger_close').click(function(){
+    $('.burger_menu').removeClass('burger_menu-active')
+    $('.menu_background').hide()
+})
+$('.what_goes').click(function(){
+    $('.what_goes_contant').addClass('what_goes-active')
     
-// }
+})
 
-// function closeRightMenu() {
-//     document.getElementById("rightMenu").style.display = "none";
+$('.what_goes-close').click(function(){
+    $('.what_goes_contant').removeClass('what_goes-active')
     
-// }
+})
+
+$('.menu_list li').click(function(){
+    $('.menu_list li').removeClass("active-menu_list")
+    $(this).addClass("active-menu_list")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
